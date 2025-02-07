477d6a8c0d828f8d0ffc105452826e74089fe043
WPExplorer <wpexplorer@gmail.com>
2025-02-07 13:01:14 -0800

    Update wp-youtube-nocookie.php

--- wp-youtube-nocookie.php
+++ wp-youtube-nocookie.php
@@ -1,43 +1,74 @@
 <?php
+/**
+ * Plugin Name:       WP YouTube Nocookie
+ * Plugin URI:        https://github.com/wpexplorer/wp-youtube-nocookie
+ * Description:       Modifies YouTube embeds to use the youtube-nocookie.com domain.
+ * Version:           1.1
+ * Requires at least: 6.6
+ * Requires PHP:      8.0
+ * Author:            WPExplorer
+ * Author URI:        https://www.wpexplorer.com/
+ * License:           GPL v3 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain:       wp-youtube-nocookie
+ */
+
 /*
-Plugin Name: WP YouTube Nocookie
-Description: Modifies YouTube embeds to use the youtube-nocookie.com domain to prevent issues with the GDPR.
-Version: 1.0
-Author: Aj Clarke
-Author URI: https://www.wpexplorer.com/
-License: GPLv2
+WP YouTube Nocookie is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+WP YouTube Nocookie is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with WP YouTube Nocookie. If not, see https://www.gnu.org/licenses/gpl-3.0.html.
 */
 
-final class WP_Youtube_No_Cookie {
+/**
+ * Prevent direct access to this file.
+ */
+defined( 'ABSPATH' ) || exit;
 
-	/**
-	 * Constructor.
-	 */
-	public function __construct() {
-		add_filter( 'embed_oembed_html', [ $this, 'filter_embed_oembed_html' ] );
-		add_filter( 'the_content', [ $this, 'filter_the_content' ], 100 );
-	}
+/**
+ * Main plugin class.
+ */
+if ( ! class_exists( 'WP_Youtube_No_Cookie' ) ) {
 
-	/**
-	 * Modify the oEmbed HTMl.
-	 */
-	public function filter_embed_oembed_html( $html ) {
-		if ( str_contains( $html, 'youtube.com' ) ) {
-			$html = str_replace( 'youtube.com', 'youtube-nocookie.com', $html );
+	class WP_Youtube_No_Cookie {
+	
+		/**
+		 * Constructor.
+		 */
+		public function __construct() {
+			add_filter( 'embed_oembed_html', [ $this, 'filter_embed_oembed_html' ] );
+			add_filter( 'the_content', [ $this, 'filter_the_content' ], 100 );
 		}
-		return $html;
-	}
-
-	/**
-	 * Modify the post content.
-	 */
-	public function filter_the_content( $content ) {
-		if ( str_contains( $content, 'youtube.com/embed' ) ) {
-			$content = str_replace( 'youtube.com/embed', 'youtube-nocookie.com/embed', $content );
+	
+		/**
+		 * Modify the oEmbed HTMl.
+		 */
+		public function filter_embed_oembed_html( $html ) {
+			if ( str_contains( $html, 'youtube.com' ) ) {
+				$html = str_replace( 'youtube.com', 'youtube-nocookie.com', $html );
+			}
+			return $html;
 		}
-		return $content;
+	
+		/**
+		 * Modify the post content.
+		 */
+		public function filter_the_content( $content ) {
+			if ( str_contains( $content, 'youtube.com/embed' ) ) {
+				$content = str_replace( 'youtube.com/embed', 'youtube-nocookie.com/embed', $content );
+			}
+			return $content;
+		}
+
 	}
 
+	new WP_Youtube_No_Cookie;
 }
-
-new WP_Youtube_No_Cookie;
