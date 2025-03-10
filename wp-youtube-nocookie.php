1384fdc8fc84cbca7cc2b6c7e73b421a68509b6c
WPExplorer <wpexplorer@gmail.com>
2025-03-10 16:36:37 -0700

    Update wp-youtube-nocookie.php

--- wp-youtube-nocookie.php
+++ wp-youtube-nocookie.php
@@ -6,8 +6,8 @@
  * Version:           1.1
  * Requires at least: 6.6
  * Requires PHP:      8.0
- * Author:            bkno
- * Author URI:        https://digitalgarden.co/
+ * Author:            WPExplorer
+ * Author URI:        https://www.wpexplorer.com/
  * License:           GPL v3 or later
  * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
  * Text Domain:       wp-youtube-nocookie
